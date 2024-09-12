dddb94fb0c37fc3bd2a7f65177cbac2240023cb2
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-13 00:13:46 +0530

    create upvote and downvote routes

---
+++ app/api/streams/upvote/route.ts
@@ -0,0 +1,36 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const UpvoteStreamSchema = z.object({
+  streamId: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const session = await getServerSession();
+
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: session?.user?.email ?? "",
+      },
+    });
+    if (!user) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 403 });
+    }
+    const data = UpvoteStreamSchema.parse(await req.json());
+
+    await prismaClient.upvote.create({
+      data: {
+        userId: user.id,
+        streamId: data.streamId,
+      },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error while upvoting" },
+      { status: 411 }
+    );
+  }
+}
