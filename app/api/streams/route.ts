5c653e75ee352486306cc1d502f834d0cdf6c311
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-13 17:24:56 +0530

    modify create stream route

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -2,9 +2,10 @@ import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
-const YT_REGEX = new RegExp(
-  "/^https?://(www.)?youtube.com/watch?v=[w-]+(&[w-]+=[w-]+)*$/"
-);
+//@ts-expect-error doesn't have types include
+import youtubesearchapi from 'youtube-search-api'
+
+const YT_REGEX = new RegExp("^(https?://)?(www.)?(youtube.com|youtu.be)/.+$");
 
 const CreateStreamSchema = z.object({
   creatorId: z.string(),
@@ -17,24 +18,46 @@ export async function POST(req: NextRequest) {
     const isYt = YT_REGEX.test(data.url);
 
     if (!isYt) {
-      return NextResponse.json(
-        { message: "Error while adding a stream" },
-        { status: 411 }
-      );
+      return NextResponse.json({ message: "Invalid URL" }, { status: 411 });
     }
 
     const extractedId = data.url.split("?v=")[1];
 
-    await prismaClient.stream.create({
+    const videoDetails = await youtubesearchapi.GetVideoDetails(extractedId);
+    const title = videoDetails.title;
+    const thumbnails = videoDetails.thumbnail.thumbnails;
+    thumbnails.sort(
+      (a: { width: number }, b: { width: number }) => a.width - b.width
+    );
+
+    const stream = await prismaClient.stream.create({
       data: {
         userId: data.creatorId,
         url: data.url,
         extractedId,
         type: "Youtube",
+        title: title ?? "Title not found",
+        smallImg:
+          (thumbnails.length > 1
+            ? thumbnails[thumbnails.length - 2].url
+            : thumbnails[thumbnails.length - 2].url) ??
+          "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg",
+        bigImg:
+          thumbnails[thumbnails.length - 1].url ??
+          "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg",
       },
     });
 
-    return NextResponse.json({ message: "Success" }, { status: 200 });
+    return NextResponse.json(
+      {
+        message: "Success",
+        id: stream.id,
+        title: stream.title,
+        smallImg: stream.smallImg,
+        bigImg: stream.bigImg,
+      },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json(
       { message: "Error while adding a stream" },
