c3829d27332b34ac5514fe200fc2556daa5d1d66
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-09-13 14:42:34 +0530

    add user to database when logging in with google

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
+import { prismaClient } from "@/app/lib/db";
 
 const handler = NextAuth({
   providers: [
@@ -8,6 +9,25 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    async signIn(params) {
+      if (!params.user.email) {
+        return false;
+      }
+      try {
+        await prismaClient.user.create({
+          data: {
+            email: params.user.email,
+            provider: "Google",
+          },
+        });
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+      return true;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
